0c69c090292a81f674d8e32ba27a00a15076ab1e
RomanDanielGutierrezz <romandanielgutierrez@gmail.com>
2025-05-03 22:19:35 -0300

    Add files via upload

---
+++ PromedioNums/PromedioNums/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromedioNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            double numero, acumulador = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                bool verificador = false;
+
+                while (!verificador)
+                {
+                    Console.WriteLine($"Ingrese nuemro {i}");
+                    string entrada = Console.ReadLine();
+                    
+                    if (double.TryParse(entrada, out numero))
+                    {
+                        acumulador += numero;
+                        contador++;
+                        verificador = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Ingrese un numero/valor valido");
+                        Console.WriteLine();
+                    }
+                }
+
+            }
+            var prom =acumulador / contador;
+            Console.WriteLine($"Su promedio de numeros es {prom}");
+        }   
+    }
+}

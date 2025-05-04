83a216a82dbe57db6adb977c209ed8c8d499698d
RomanDanielGutierrezz <romandanielgutierrez@gmail.com>
2025-05-03 22:18:11 -0300

    EJ4_divisibles

--- Program.cs
+++ Program.cs
@@ -1,44 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PromedioNums
+namespace Divisibles_por_2_y_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int contador = 0;
-            double numero, acumulador = 0;
-            for (int i = 1; i <= 10; i++)
+            List<int> valores = new List<int> {0, 1};
+            Console.WriteLine("Indique el valor inicial");
+            valores[0] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Indique el valor Final");
+            valores[1] = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int numero = valores[0]; numero <= valores[1]; numero++)
             {
-                bool verificador = false;
-
-                while (!verificador)
+                if (numero % 2 == 0 && numero % 3 == 0)
                 {
-                    Console.WriteLine($"Ingrese nuemro {i}");
-                    string entrada = Console.ReadLine();
-                    
-                    if (double.TryParse(entrada, out numero))
-                    {
-                        acumulador += numero;
-                        contador++;
-                        verificador = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Ingrese un numero/valor valido");
-                        Console.WriteLine();
-                    }
+                    Console.WriteLine(numero);
                 }
-
             }
-            var prom =acumulador / contador;
-            Console.WriteLine($"Su promedio de numeros es {prom}");
-        }   
+        }
     }
 }
